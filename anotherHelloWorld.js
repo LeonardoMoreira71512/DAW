adab055fb2457a72f51b4d940bac9a6c9f16e21d
Lennayyy <73541881+LeonardoMoreira71512@users.noreply.github.com>
2022-10-12 12:35:41 +0100

    Delete anotherHelloWorld.js

--- anotherHelloWorld.js
+++
@@ -1,8 +0,0 @@
-function greeter(person) {
-    return "Hello " + person;
-}
-var user = "fofinho"; //if you remove the type, TypeScript will infer
-var message = greeter(user);
-var heading = document.createElement("h1");
-heading.textContent = message;
-document.body.appendChild(heading);
